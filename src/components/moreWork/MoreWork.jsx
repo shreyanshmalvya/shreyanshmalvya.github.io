0e3dff0cc24a7372ce69c9bc4b4aad32ceab3356
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-04-01 23:45:08 +0530

    updated basic code for morework and footer

---
+++ src/components/moreWork/MoreWork.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './moreWork.scss'
+
+
+const MoreWork = () => {
+    return (
+        <div className="moreWorkWrapper">
+            <div className="header">
+                <div className="title">
+                    More Work
+                </div>
+                <div className="info">
+                    On the side, I like to pick up fun branding or illustration projects.
+                </div>
+            </div>
+            <div className="flexWrapper">
+                <div className="projectA">
+                    <img src={ } alt='projectA' />
+                    <div className="projectTitle">ARTS GROOVES AND FOODS • FALL 2021</div>
+                </div>
+                <div className="projectB">
+                    <img src={ } alt='projectB' />
+                    <div className="projectTitle">ARTS GROOVES AND FOODS • FALL 2021</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MoreWork
