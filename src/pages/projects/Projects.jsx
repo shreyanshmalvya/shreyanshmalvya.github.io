ae66b8861da4c2a90c197c9a9fd5759850a7c623
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-04-01 21:11:33 +0530

    Updated website w projects section

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Card from '../../components/projectCards/Card'
+import Cover1 from '../../images/cover1.jpg'
 import Cover2 from '../../images/cover2.png'
+import Cover3 from '../../images/cover3.png'
 
 const Projects = () => {
   return (
     <div>
-        <Card image = {Cover2} color= '#000000' title= 'Thrifty AI' pos='Lead Graphic Designer · Website Devlopment Lead' desc= 'Website Revamp with custom designs and all new framework implementation' />
+        <Card image = {Cover1} color= '#000000' title= 'Thrifty AI' pos='Lead Graphic Designer · Website Devlopment Lead' desc= 'Website Revamp with custom designs and all new framework implementation' />
         <Card image = {Cover2} color= '#141415' title= 'Thrifty AI' pos='Lead Graphic Designer · Website Devlopment Lead' desc= 'Website Revamp with custom designs and all new framework implementation'/>
-        <Card image = {Cover2} color= '#000000' title= 'Thrifty AI' pos='Lead Graphic Designer · Website Devlopment Lead' desc= 'Website Revamp with custom designs and all new framework implementation' />
+        <Card image = {Cover3} color= '#000000' title= 'Thrifty AI' pos='Lead Graphic Designer · Website Devlopment Lead' desc= 'Website Revamp with custom designs and all new framework implementation' />
     </div>
   )
 }
